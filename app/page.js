32f2a4a620a834400b75f80dc9282f77d1bcd6e1
PlatypusKing89 <146495944+PlatypusKing89@users.noreply.github.com>
2026-09-10 00:49:08 -0400

    Apply final Issue 0 prediction and history touches

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import pageCss from './content/css0';
 import sourceHtml from './content/body';
 import updateIssue from './content/issue0-updates';
+import finalizeIssue from './content/issue0-final-touches';
 
 export default function Page() {
-  const pageHtml = updateIssue(sourceHtml);
+  const pageHtml = finalizeIssue(updateIssue(sourceHtml));
   return (
     <>
       <style dangerouslySetInnerHTML={{ __html: pageCss }} />
