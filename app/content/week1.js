18733add46069254e4c1b456d888789d08198eba
PlatypusKing89 <146495944+PlatypusKing89@users.noreply.github.com>
2026-09-15 13:15:57 -0400

    Fix Week 1 image asset paths

--- app/content/week1.js
+++ app/content/week1.js
@@ -5,11 +5,9 @@ import body2 from './week1-body-part2';
 import body3 from './week1-body-part3';
 import body4 from './week1-body-part4';
 import body5 from './week1-body-part5';
-import { week1Hero } from './week1-hero-data';
-import { week1Cartoon } from './week1-cartoon-data';
 
 export const week1Css = css1 + css2;
 
 export const week1Html = (body1 + body2 + body3 + body4 + body5)
-  .replace('__WEEK1_HERO__', week1Hero)
-  .replace('__WEEK1_CARTOON__', week1Cartoon);
+  .replace('__WEEK1_HERO__', '/a_cinematic_stylized_comic_book_illustration_lik.png')
+  .replace('__WEEK1_CARTOON__', '/a_wide_detailed_comic_illustration_style_split_s.png');
