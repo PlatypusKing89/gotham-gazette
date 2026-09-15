923dd72fbc291de8cd5d324a64e31901e73c4360
PlatypusKing89 <146495944+PlatypusKing89@users.noreply.github.com>
2026-09-15 13:52:24 -0400

    Add Week 1 Gazette artwork

--- app/content/week1.js
+++ app/content/week1.js
@@ -9,5 +9,5 @@ import body5 from './week1-body-part5';
 export const week1Css = css1 + css2;
 
 export const week1Html = (body1 + body2 + body3 + body4 + body5)
-  .replace('__WEEK1_HERO__', '/a_cinematic_stylized_comic_book_illustration_lik.png')
-  .replace('__WEEK1_CARTOON__', '/a_wide_detailed_comic_illustration_style_split_s.png');
+  .replace('__WEEK1_HERO__', '/week-1-hero.webp')
+  .replace('__WEEK1_CARTOON__', '/week-1-cartoon.webp');
